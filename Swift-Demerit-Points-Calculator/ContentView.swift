27f9f9ff722ccd3414907ddf471d290d51adf2c8
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-06-07 11:54:10 +1200

    Created main View

--- Swift-Demerit-Points-Calculator/ContentView.swift
+++ Swift-Demerit-Points-Calculator/ContentView.swift
@@ -8,15 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var drivingSpeed: String = ""
+    @State private var speedLimit: String = ""
+    @State private var holidayPeriod: Bool = false
+    @State private var mandatoryPenalty: Bool = false
+    @State private var penaltyPoints: Int = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world! This is a test!")
+            
+            TextField("Driving Speed", text: $drivingSpeed)
+                .padding()
+            
+            TextField("Speed Limit", text: $speedLimit)
+                .padding()
+            
+            Toggle("Holiday Period", isOn: $holidayPeriod)
+                .padding()
+            
+            Button("Calculate") {
+                calculateDemeritPoints()
+            }
+            .padding()
+            
+            Text("Mandatory Penalty: \(mandatoryPenalty ? "True" : "False")")
+            Text("Penalty Points: \(penaltyPoints)")
         }
         .padding()
     }
+    
+    private func calculateDemeritPoints() {
+        guard let drivingSpeedValue = Double(drivingSpeed),
+              let speedLimitValue = Int(speedLimit) else {
+            return // What to return here?
+        }
+        
+        let result = getDemeritPoints(drivingSpeed: drivingSpeedValue, speedLimit: speedLimitValue, holidayPeriod: holidayPeriod)
+        mandatoryPenalty = result.mandatoryPenalty
+        penaltyPoints = result.penaltyPoints
+    }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
