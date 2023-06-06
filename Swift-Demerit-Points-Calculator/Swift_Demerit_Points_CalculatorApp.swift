6bac46e4756914b8fed0a2cfaa536322b3279793
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-06-07 11:27:34 +1200

    Initial Commit

---
+++ Swift-Demerit-Points-Calculator/Swift_Demerit_Points_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Demerit_Points_CalculatorApp.swift
+//  Swift-Demerit-Points-Calculator
+//
+//  Created by Flynn Stevens on 7/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Demerit_Points_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
